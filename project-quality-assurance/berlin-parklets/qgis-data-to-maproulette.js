82d0b0e620f7778280f86bf904c60a53ffbcb87d
Tobias <tobias@fixmycity.de>
2022-07-03 21:37:54 +0200

    QA project parklet: Fix markdown for task description

--- project-quality-assurance/berlin-parklets/qgis-data-to-maproulette.js
+++ project-quality-assurance/berlin-parklets/qgis-data-to-maproulette.js
@@ -13,62 +13,74 @@ fs.readFile("./qgis.geojson", "utf8", (err, data) => {
   const processedData = {
     type: "FeatureCollection",
     features: rawData.features.map((f) => {
-      let task = "";
+      let task = [""];
 
-      const taskPartMapillary = `\n\n
-      **Mapillary:**\n
-      [Mapillary an diesem Ort öffnen](https://www.mapillary.com/app/?lat=${f.geometry.coordinates[1]}&lng=${f.geometry.coordinates[1]}&z=17&dateFrom=2022-01-01)\n
-      [Verknüpftes Mapillary-Bild](https://www.mapillary.com/app/?pKey=${f.properties.JOIN_mapillary})\n
-      `;
+      const taskPartMapillary = [
+        "",
+        "",
+        "**Mapillary:**",
+        `[Mapillary an diesem Ort öffnen](https://www.mapillary.com/app/?lat=${f.geometry.coordinates[1]}&lng=${f.geometry.coordinates[1]}&z=17&dateFrom=2022-01-01)`,
+        `[Verknüpftes Mapillary-Bild](https://www.mapillary.com/app/?pKey=${f.properties.JOIN_mapillary})`,
+      ];
 
-      const taskPartTagging = `\n\n
-      **Tagging-Tipps:**\n
-      * Parklet: Eine Fläche mit \`leisure=parket\` + \`check_date\`\n
-      * Planter: Eine Fläche mit \`man_made=planter\` + \`check_date\` + \`planter:position=lane\` oder \`street_side\` (Parkbucht)\n
-      `;
+      const taskPartTagging = [
+        "",
+        "",
+        "**Mapillary:**",
+        "* Parklet: Eine Fläche mit `leisure=parket` + `check_date`",
+        "* Planter: Eine Fläche mit `man_made=planter` + `check_date` + `planter:position=lane` oder `street_side` (Parkbucht)",
+      ];
 
       if (f.NUM_OSM_IN_EXT_DATA > 0) {
         // Found someting in OSM nearby the source data.
         if (f.JOIN_leisure === "parklet") {
-          task = `Hier gibt es laut externen Daten ein Parklet.\n
-            Wir haben im Umkreis ein Parket gefunden.\n
-            Bitte prüfe die Tags, ergänze einen \`mapillary\`-Tag (wenn möglich)
-            und ergänze gerne ein \`check_date\` (ggf. basierend auf dem Mapillary Bild-Datum).\n`;
+          task = [
+            "Hier gibt es laut externen Daten ein Parklet.",
+            "Wir haben im Umkreis ein Parket gefunden.",
+            `Bitte prüfe die Tags, ergänze einen \`mapillary\`-Tag (wenn möglich)
+            und ergänze gerne ein \`check_date\` (ggf. basierend auf dem Mapillary Bild-Datum).`,
+          ];
         } else if (f.JOIN_man_made === "planter") {
-          task = `Hier gibt es laut externen Daten ein Parklet.\n
-            Wir haben im Umkreis einen \`man_made=planter\` gefunden.\n
-            Bitte prüfe, ob dieser Planter an der Stelle gemapped ist, wo das Parklet steht.\n
-            Bitte füge \`planter:position=lane\` oder \`street_side\` hinzu,
-            damit er automatisch aus dem Parkstreifen ausgestanzt werden kann.\n
-            Bitte prüfe die Tags, ergänze einen \`mapillary\`-Tag (wenn möglich)
-            und ergänze gerne ein \`check_date\` (ggf. basierend auf dem Mapillary Bild-Datum).\n
-            ${taskPartMapillary}
-            ${taskPartTagging}`;
+          task = [
+            "Hier gibt es laut externen Daten ein Parklet.",
+            "Wir haben im Umkreis einen `man_made=planter` gefunden.",
+            "Bitte prüfe, ob dieser Planter an der Stelle gemapped ist, wo das Parklet steht.",
+            `Bitte füge \`planter:position=lane\` oder \`street_side\` hinzu,
+            damit er automatisch aus dem Parkstreifen ausgestanzt werden kann.`,
+            `Bitte prüfe die Tags, ergänze einen \`mapillary\`-Tag (wenn möglich)
+            und ergänze gerne ein \`check_date\` (ggf. basierend auf dem Mapillary Bild-Datum).`,
+            ...taskPartMapillary,
+            ...taskPartTagging,
+          ];
         } else if (
           f.JOIN_outdoor_seating === "parket" &&
           f.JOIN_leisure === "outdoor_seating"
         ) {
-          task = `Hier gibt es laut externen Daten ein Parklet.\n
-            Wir haben im Umkreis ein \`leisure=outdoor_seating\`+\`outdoor_seatig=parklet\` gefunden.\n
-            Bitte prüfe, ob diese Fläche für Außengastronomie an der Stelle gemapped ist, wo das Parklet steht.\n
-            Bitte prüfe die Tags, ergänze einen \`mapillary\`-Tag (wenn möglich)
-            und ergänze gerne ein \`check_date\` (ggf. basierend auf dem Mapillary Bild-Datum).\n
-            ${taskPartMapillary}
-            ${taskPartTagging}`;
+          task = [
+            "Hier gibt es laut externen Daten ein Parklet.",
+            "Wir haben im Umkreis ein `leisure=outdoor_seating`+`outdoor_seatig=parklet` gefunden.",
+            "Bitte prüfe, ob diese Fläche für Außengastronomie an der Stelle gemapped ist, wo das Parklet steht.",
+            `Bitte prüfe die Tags, ergänze einen \`mapillary\`-Tag (wenn möglich)
+            und ergänze gerne ein \`check_date\` (ggf. basierend auf dem Mapillary Bild-Datum).`,
+            ...taskPartMapillary,
+            ...taskPartTagging,
+          ];
         } else {
-          task = "TODO: OSM data found but no match in which data.";
+          task = ["TODO: OSM data found but no match in which data."];
         }
       } else {
-        task = `Wir haben im Umkreis kein Parklet oder Planter gefunden. Bitte trage einen ein.\n
-            ${taskPartMapillary}
-            ${taskPartTagging}`;
+        task = [
+          "Wir haben im Umkreis kein Parklet oder Planter gefunden. Bitte trage einen ein.",
+          ...taskPartMapillary,
+          ...taskPartTagging,
+        ];
       }
 
       return {
         type: "Feature",
         geometry: f.geometry,
         properties: {
-          task: task,
+          task: task.join(" \n"),
           ...Object.fromEntries(
             Object.entries(f.properties).filter(([_k, v]) => v !== null)
           ),
