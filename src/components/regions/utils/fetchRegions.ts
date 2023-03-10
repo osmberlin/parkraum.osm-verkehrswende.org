329bd8fbb234b848ea6bd5fe922efca5de539a15
Tobias <tobias@fixmycity.de>
2023-03-10 09:30:35 +0100

    fetchRegions extrahiert in utils helper

---
+++ src/components/regions/utils/fetchRegions.ts
@@ -0,0 +1,53 @@
+import { Octokit } from '@octokit/rest'
+import { type BBox, bbox, centerOfMass } from '@turf/turf'
+import axios from 'axios'
+
+type GithubFile = { filename: string; downloadUrl: string; url: string; content: null }
+type GithubFileWithContent = GithubFile & {
+  slug: string
+  name: string
+  bounds: BBox
+  content: any
+}
+
+export const fetchRegions = async () => {
+  const octokit = new Octokit()
+  const githubContent = await octokit.repos.getContent({
+    owner: 'osmberlin',
+    repo: 'osm-parking-processing',
+    ref: 'docker', // For now, until the branch is merged into `main`
+    path: '/extracts/boundaries',
+  })
+
+  const githubFiles = githubContent.data
+    // @ts-ignore no idea how to guard this from TS
+    ?.filter((file: any) => file.type === 'file')
+    ?.map((file: any) => {
+      return {
+        filename: file.name,
+        downloadUrl: file.download_url,
+        url: file.html_url,
+        content: null,
+      }
+    }) as GithubFile[]
+
+  return githubFiles
+}
+
+export const fetchRegionWithContent = async () => {
+  const githubFiles = await fetchRegions()
+  const githubFilesWithContent = (await Promise.all(
+    githubFiles.map(async (file: any) => {
+      const resp = await axios.get(file.downloadUrl)
+      const content = await resp.data
+      const slug = file.filename.replace('.geojson', '')
+      const name = slug.charAt(0).toUpperCase() + slug.slice(1)
+      const bounds = bbox(content)
+      const center = centerOfMass(content)
+
+      return { ...file, slug, name, content, bounds, center }
+    })
+  )) as GithubFileWithContent[]
+
+  return githubFilesWithContent
+}
