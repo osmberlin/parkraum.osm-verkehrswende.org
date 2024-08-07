8aa4d9ac2f5a01361feabd3d68b04f7eaf38cc97
Tobias <tobias@fixmycity.de>
2024-08-07 10:51:21 +0200

    Map: Fix types

--- src/components/regions/utils/atlasUrl.ts
+++ src/components/regions/utils/atlasUrl.ts
@@ -1,4 +1,4 @@
-export const atlasUrl = (region: string, center: [number, number]) => {
+export const atlasUrl = (region: string, center: [number, number] | number[]) => {
   if (region === 'berlin') {
     // We have a special case for Berlin where we use a different region with has additional public data from the city administration associated
     return `https://radverkehrsatlas.de/regionen/parkraum-berlin?lat=${center[1]}&lng=${center[0]}`
