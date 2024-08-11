27123604f6a15a4ac5d1488f5b70c4d7432e20ee
Tobias <tobias@fixmycity.de>
2024-08-11 05:09:32 +0200

    Fix map URLs after refactoring
    It has to be lng, lat…

--- src/components/regions/utils/atlasUrl.ts
+++ src/components/regions/utils/atlasUrl.ts
@@ -1,7 +1,7 @@
 export const atlasUrl = (region: string, center: [number, number] | number[]) => {
   if (region === 'berlin') {
     // We have a special case for Berlin where we use a different region with has additional public data from the city administration associated
-    return `https://radverkehrsatlas.de/regionen/parkraum-berlin?map=12/${center[0]}/${center[1]}`
+    return `https://radverkehrsatlas.de/regionen/parkraum-berlin?map=12/${center[1]}/${center[0]}`
   }
-  return `https://radverkehrsatlas.de/regionen/parkraum?map=12/${center[0]}/${center[1]}`
+  return `https://radverkehrsatlas.de/regionen/parkraum?map=12/${center[1]}/${center[0]}`
 }
